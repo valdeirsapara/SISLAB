fb549e371b2cda8bf2253d196bc83003b79053d7
Valdeir Sampaio Sapará Filho <valdeir@srv764519.hstgr.cloud>
2025-03-30 04:46:41 +0000

    atualizacao do docker compose override prod e atualizando o pip antes de instalar as depedencias do python

--- config/settings/comum.py
+++ config/settings/comum.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'data' / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
