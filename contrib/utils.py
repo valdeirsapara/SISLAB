a624084f8302095012092833d1619b087a372d4e
Valdeir Sapará <111082549+valdeirsapara@users.noreply.github.com>
2025-06-06 12:39:33 -0400

    Fix unique_username to ensure uniqueness (#3)

--- contrib/utils.py
+++ contrib/utils.py
@@ -1,2 +1,13 @@
+from django.contrib.auth import get_user_model
+
+
 def unique_username(user):
-    return f'{user.first_name}_{user.last_name}'
+    """Generate a unique username based on first and last name."""
+    base_username = f"{user.first_name}_{user.last_name}".lower().replace(" ", "_")
+    username = base_username
+    UserModel = get_user_model()
+    counter = 1
+    while UserModel.objects.filter(username=username).exists():
+        counter += 1
+        username = f"{base_username}_{counter}"
+    return username
