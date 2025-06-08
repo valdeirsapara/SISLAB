fdcd8d27917cc50fd4f50649b75c64de1ad75290
Valdeir Sapará <111082549+valdeirsapara@users.noreply.github.com>
2025-06-08 11:59:45 -0400

    Add unique_username test (#4)

--- contrib/tests.py
+++ contrib/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .utils import unique_username
+
+
+class UniqueUsernameTests(TestCase):
+    """Tests for the ``unique_username`` utility."""
+
+    def test_unique_username_simple(self):
+        user = User(first_name="First", last_name="Last")
+        self.assertEqual(unique_username(user), "first_last")
+
